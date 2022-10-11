23475c4d3903978271c95783c9a7a1170fd69984
ananduremanan <ananduor@gmail.com>
2022-10-11 10:35:45 +0530

    Minor Changes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
 from app import app
 
+from flask import render_template
 
-@app.route('/')
-def main_view():
-    return "Hello World!"
+
+@app.route("/")
+def index():
+    return render_template("public/index.html")
+
+@app.route("/jinja")
+def jinja():
+    name = "Anandhu Remanan"
+
+    subjects = ["Computer Science", "Math", "English", "Malayalam"]
+
+    return render_template("public/jinja.html", name=name, subjects=subjects)
